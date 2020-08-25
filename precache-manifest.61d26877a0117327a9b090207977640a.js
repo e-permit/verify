542444211ce258a229e8f18a2c1a4c71595e0860
Adem Çağlın <adem.caglin@uab.gov.tr>
2020-08-25 14:02:37 +0300

    Updates

---
+++ precache-manifest.61d26877a0117327a9b090207977640a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13187fb68e4f26cbece6ef0b72f8f68",
+    "url": "/verify/index.html"
+  },
+  {
+    "revision": "bfa1ff64585f75714fa4",
+    "url": "/verify/static/js/2.fd331470.chunk.js"
+  },
+  {
+    "revision": "21c42f9669a54e0265d224c6dbbb369d",
+    "url": "/verify/static/js/2.fd331470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44d92fce7770b359a78",
+    "url": "/verify/static/js/main.f8ed9a79.chunk.js"
+  },
+  {
+    "revision": "67fdb63c053f8400d0e7",
+    "url": "/verify/static/js/runtime-main.ad92eaef.js"
+  }
+]);
